e567df17a8e9c1dd2308140fa73cd5dcc025d0ec
yykrn <resulyaykiran@gmail.com>
2024-04-06 17:11:25 +0300

    bootstrap + sass backend     passwordencoder

--- frontend/src/pages/SignUp/index.jsx
+++ frontend/src/pages/SignUp/index.jsx
@@ -10,7 +10,7 @@ export function SignUp() {
 
     const onSubmit = (event) => {
         event.preventDefault();
-        axios.post("/api/v1/users",{
+        axios.post("/api/v1/users", {
             username,
             email,
             password
@@ -18,24 +18,34 @@ export function SignUp() {
 
     }
 
-    return <form onSubmit={onSubmit}>
-        <h1>Sign Up</h1>
-        <div>
-            <label htmlFor="username">Username</label>
-            <input id="username" onInput={(event) => setUsername(event.target.value)} />
+    return <div className="container">
+        <div className="row justify-content-center">
+            <div className="col-6">
+                <form className="card" onSubmit={onSubmit}>
+                    <div className="card-header">
+                        <h1>Sign Up</h1>
+                    </div>
+                    <div className="card-body">
+                        <div className="mb-3">
+                            <label htmlFor="username" className="form-label">Username</label>
+                            <input id="username" className="form-control" onInput={(event) => setUsername(event.target.value)} />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="email" className="form-label">E-mail</label>
+                            <input id="email" className="form-control" onInput={(event) => setEmail(event.target.value)} />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="password" className="form-label">Password</label>
+                            <input id="password" type="password" className="form-control" onInput={(event) => setPassword(event.target.value)} />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="passwordRepeat" className="form-label">Password Repeat</label>
+                            <input id="passwordRepeat" type="password" className="form-control" onInput={(event) => setPasswordRepeat(event.target.value)} />
+                        </div>
+                        <button className="btn btn-primary" disabled={!password || password !== passwordRepeat} >Sign Up</button>
+                    </div>
+                </form>
+            </div>
         </div>
-        <div>
-            <label htmlFor="email">E-mail</label>
-            <input id="email" onInput={(event) => setEmail(event.target.value)} />
-        </div>
-        <div>
-            <label htmlFor="password">Password</label>
-            <input id="password" type="password" onInput={(event) => setPassword(event.target.value)}  />
-        </div>
-        <div>
-            <label htmlFor="passwordRepeat">Password Repeat</label>
-            <input id="passwordRepeat" type="password" onInput={(event) => setPasswordRepeat(event.target.value)}  />
-        </div>
-        <button disabled={!password || password !== passwordRepeat} >Sign Up</button>
-    </form>
+    </div>
 }
--- ws/src/main/java/com/ecom/ws/user/UserController.java
+++ ws/src/main/java/com/ecom/ws/user/UserController.java
@@ -1,18 +1,15 @@
 package com.ecom.ws.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ecom.ws.UserRepository;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 import com.ecom.ws.shared.GenericMessage;
-
 
 @RestController
 public class UserController {
@@ -20,16 +17,18 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
     @GetMapping("/api/v1/users")
-    public String getUsers(@RequestParam(required=false) String param) {
+    public String getUsers(@RequestParam(required = false) String param) {
         return new String("hello users");
     }
 
     @PostMapping("/api/v1/users")
     public GenericMessage postUser(@RequestBody User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userService.save(user);
         return new GenericMessage("User created");
     }
-    
-    
+
 }
